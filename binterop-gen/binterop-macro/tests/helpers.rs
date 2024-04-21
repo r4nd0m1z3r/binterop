20dfec17bb74bd300d1fb22683b4e393f2f4685e
r4nd0m1z3r <dimazin2004@gmail.com>
2024-04-21 23:41:43 +0300

    Proper trait impls for Vector.

--- binterop-gen/binterop-macro/tests/helpers.rs
+++ binterop-gen/binterop-macro/tests/helpers.rs
@@ -1,10 +1,27 @@
 #![allow(dead_code)]
 
-#[derive(Copy, Clone, Debug)]
+use std::fmt::Debug;
+
 pub struct Vector<T> {
     pub ptr: *mut T,
     pub len: u64,
     pub capacity: u64,
+}
+impl<T: Debug> Debug for Vector<T> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_list().entries(self.as_slice()).finish()
+    }
+}
+impl<T: Clone> Clone for Vector<T> {
+    fn clone(&self) -> Self {
+        let mut new = Self::with_capacity(self.capacity);
+
+        let new_slice = new.as_mut_slice();
+        new_slice.clone_from_slice(self.as_slice());
+        new.len = self.len;
+
+        new
+    }
 }
 impl<T> Vector<T> {
     pub fn new() -> Self {
@@ -16,6 +33,24 @@ impl<T> Vector<T> {
             capacity: vec.capacity() as u64,
         }
     }
+
+    fn update_from_vec(&mut self, vec: Vec<T>) {
+        let (ptr, len, capacity) = vec.into_raw_parts();
+        self.ptr = ptr;
+        self.len = len as u64;
+        self.capacity = capacity as u64;
+    }
+
+    pub fn with_capacity(capacity: u64) -> Self {
+        let mut vec = Vec::with_capacity(capacity as usize);
+
+        Self {
+            ptr: vec.as_mut_ptr(),
+            len: vec.len() as u64,
+            capacity: vec.capacity() as u64,
+        }
+    }
+
     pub fn as_slice(&self) -> &[T] {
         unsafe { std::slice::from_raw_parts(self.ptr, self.len as usize) }
     }
@@ -24,15 +59,20 @@ impl<T> Vector<T> {
         unsafe { std::slice::from_raw_parts_mut(self.ptr, self.len as usize) }
     }
 
+    pub fn reserve(&mut self, additional: u64) {
+        let mut vec =
+            unsafe { Vec::from_raw_parts(self.ptr, self.len as usize, self.capacity as usize) };
+        vec.reserve(additional as usize);
+
+        self.update_from_vec(vec);
+    }
+
     pub fn push(&mut self, elem: T) {
         let mut vec =
             unsafe { Vec::from_raw_parts(self.ptr, self.len as usize, self.capacity as usize) };
         vec.push(elem);
 
-        let (ptr, len, capacity) = vec.into_raw_parts();
-        self.ptr = ptr;
-        self.len = len as u64;
-        self.capacity = capacity as u64;
+        self.update_from_vec(vec);
     }
 
     pub fn pop(&mut self) -> Option<T> {
@@ -40,10 +80,7 @@ impl<T> Vector<T> {
             unsafe { Vec::from_raw_parts(self.ptr, self.len as usize, self.capacity as usize) };
         let elem = vec.pop();
 
-        let (ptr, len, capacity) = vec.into_raw_parts();
-        self.ptr = ptr;
-        self.len = len as u64;
-        self.capacity = capacity as u64;
+        self.update_from_vec(vec);
 
         elem
     }
